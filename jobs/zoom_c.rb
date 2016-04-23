da1b184902b77412b502197414c22da4eb36667c
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-04-22 23:02:49 -0700

    get zoom component widget working every 1m

--- jobs/zoom_c.rb
+++ jobs/zoom_c.rb
@@ -1,4 +1,21 @@
+require 'httparty'
+
+url = "https://14qjgk812kgk.statuspage.io/api/v2/summary.json"
+
+
+
+
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('widget_id', { })
+
+	response = HTTParty.get(url)
+
+	response = response.parsed_response
+
+	component = response["components"]
+
+	send_event('zoom_c', { items: component } )
+
+	puts "component has been fetched!"
+ 
 end
