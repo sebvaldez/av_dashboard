37cd92d88719491974af67f4266fadd6e7d3e027
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-04-30 21:29:42 -0700

    Fix on schedule_maintenance endpoint, widget displays IF data or NO data

--- jobs/scheduled_maintenance.rb
+++ jobs/scheduled_maintenance.rb
@@ -1,9 +1,7 @@
-
-# Upcoming maintenance 
+# Upcoming maintenance JSON URL ENPOINT
 url = "http://14qjgk812kgk.statuspage.io/api/v2/scheduled-maintenances/upcoming.json"
 
-
-#formatTime function
+# FormatTime function
 def fixTime (time)
 	require 'date'
 	time = DateTime.parse(time)
@@ -18,34 +16,43 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 maintenance = HTTParty.get(url)
 maintenance = maintenance.parsed_response
 
-maintenance = maintenance["scheduled_maintenances"][0]
-
 # Create empty hashed for loop below
 upcoming = Hash.new
 incidents = Hash.new
-
-# puts key and values into the "Local" upcoming hash
-if maintenance
+unless maintenance["scheduled_maintenances"].empty?
+	maintenance = maintenance["scheduled_maintenances"][0]
+	# Puts key and values into the "Local" upcoming hash
 	maintenance.each do |key, val|
 		upcoming["#{key}"] = val
-		#puts "key: #{key} val: #{val}"
 	end
 
-	#fix time formatted from JSON return
+	# Fix time formatted from JSON return
 	upcoming["scheduled_for"] = fixTime(upcoming["scheduled_for"])
 	upcoming["scheduled_until"] = fixTime(upcoming["scheduled_until"])
 
-else 
-	puts "there was an error"
+	# Put incidents array from upcoming in its own hash
+	upcoming["incident_updates"][0].each do |key, val|
+		incidents["#{key}"] = val
+	end
+else
+	# Populate hashes if there is no maintence scheduled
+	upcoming = { 'name'=> 'Maintenance', 'status'=> 'No upcoming maintenance', 'scheduled_for'=> 'none', 'scheduled_until'=> 'none'}
+	incidents = { 'body'=> 'Zoom has returned that there will be no upcoming maintenance' }
 end
+puts "Maintenance & Incidents have been fetched!"
+print "\n"
+print "\n"
+		# Debugging pring to console
+		print "upcoming hash: "
+		print "\n"
+		print upcoming
+		print "\n"
+		print "\n"
+		print "incidents hash: "
+		print "\n"
+		print incidents
 
 
-# Put incidents array from upcoming in its own hash
-upcoming["incident_updates"][0].each do |key, val|
-	incidents["#{key}"] = val
-end
-
-  send_event('scheduled_main', { items: upcoming, incident: incidents } )
-  puts "Maintenance & Incidents have been fetched!"
+send_event('scheduled_main', { items: upcoming, incident: incidents } )
 
 end
--- jobs/zoom_c.rb
+++ jobs/zoom_c.rb
@@ -8,21 +8,37 @@ url = "https://14qjgk812kgk.statuspage.io/api/v2/summary.json"
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
 
-	response = HTTParty.get(url)
+	# response = HTTParty.get(url)
 
-	response = response.parsed_response
 
-	component = response["components"]
+	# components = response["components"]
 
-	unless component.empty?
-		
-		if component["status"] == "operational"
-			#component["icon"] == arrow.
-		else
-		end
+	# test = []
 
-	send_event('zoom_c', { items: component } )
+	# 	components.each do |index|
+			
+	# 		test = index.delete_if {|x| x = index["group_id"] }
+				
+	# 	end
+			
+	#components.map { |x| puts x['group_id'] }
 
-	puts "Components have been fetched!"
+	# components.each do |index|
+
+	# 	if index["status"] == "operational"
+	# 		components['icon'] = 'Green Check'
+	# 	else
+	# 		components['icon'] = 'Red X'
+	# 	end
+	# end
+	
+	# print "new elements added from validation of status"
+	# print "\n"
+	# print components['icon']
+	# print "\n"
+
+	#send_event('zoom_c', { items: component } )
+
+	#puts "Components have been fetched!"
  
 end
