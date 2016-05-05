5b70c8ccfcc5de753f8d7a77abdaede4a441727d
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-05-05 16:36:33 -0700

    Create incident job and widget, add to av dashboard

---
+++ jobs/incidents.rb
@@ -0,0 +1,49 @@
+require 'JSON'
+require './lib/methods.rb'
+require 'time'
+
+url = "https://14qjgk812kgk.statuspage.io/api/v2/incidents/unresolved.json"
+
+
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '1m', :first_in => 0 do |job|
+
+# f = File.read('incident_test.json')
+# test = JSON.parse(f)
+
+# Get and parse upcoming incidents JSON
+response = HTTParty.get(url)
+response = response.parsed_response
+
+incident = Hash.new
+
+unless response['incidents'].empty?
+
+	test.each do |item|
+
+		incident['icon'] = 'fa fa-exclamation-circle'
+
+		incident['name'] = test['incidents'][0]['name']
+		incident['created_at'] = test['incidents'][0]['created_at']
+		incident['impact'] = test['incidents'][0]['impact']
+		incident['body'] = test['incidents'][0]['incident_updates'][0]['body']
+	end
+
+	# Use Fix time method
+	incident['created_at'] = fixTime(incident['created_at'])
+
+else
+
+	timeStamp = Time.now
+	time = fixTime(timeStamp.to_s)
+
+	incident['icon'] = 'fa fa-check-circle'
+	incident['name'] = 'No Incidents!'
+	incident['created_at'] = "#{time}"
+	incident['impact'] = 'N/A'
+	incident['body'] = "Zoom has no unresolved incidents"
+
+end
+
+  send_event('incidents', {incident: incident} )
+end
