171d180e69c89a44184c920c3cd80dedd27a5474
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-05-21 00:29:29 -0700

    speed up request by using total_records property, rather than loop count

--- jobs/total_users.rb
+++ jobs/total_users.rb
@@ -3,11 +3,11 @@ require './lib/zoomAPI.rb'
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
 
-	totalUsers = zoomUsers
-	totalUsers =totalUsers.size
-	print totalUsers
+	users = zoomAPI( 'v1/user/list', { :page_count=>30, :page_number=>1 } )
+	response = HTTParty.post(users)
+	response = response.parsed_response
 
-
+	totalUsers = response['total_records']
 
   send_event('total_users', {count: totalUsers} )
 end
