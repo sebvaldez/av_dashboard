7375c1625a4565b54b6bdae8c77f64bb0f1ade13
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-06-04 12:18:37 -0700

    Call every 5m

--- jobs/total_users.rb
+++ jobs/total_users.rb
@@ -3,11 +3,10 @@ require './lib/zoomAPI.rb'
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
 
-	users = zoomAPI( 'v1/user/list', { :page_count=>30, :page_number=>1 } )
+	users = zoomAPI( 'v1/user/list', { :page_size=>30, :page_number=>1 } )
 	response = HTTParty.post(users)
 	response = response.parsed_response
 
 	totalUsers = response['total_records']
-
   send_event('total_users', {count: totalUsers} )
 end
