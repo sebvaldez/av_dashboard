f23df887f76b00672c4d0b18c33352beb5f5aa51
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-06-04 12:24:22 -0700

    every 5h calculate total bill and cost per user

--- jobs/est_bill_cost.rb
+++ jobs/est_bill_cost.rb
@@ -1,79 +1,70 @@
 require'./lib/zoomAPI.rb'
-require 'pry'
 
-# Variables that are constant prices
+# Constants in Bill
 constants = {
 	:user_block => 32625.00,
-
 	:large_meeting => 5773.90,
-
 	:webinar_block => 9600.00,
-
 	:room_connector_block => 490.00,
-
 	:audio_commit_block	=> 3000.00,
-
 	:cloud_block => 500.00
 }
 
-estimated_cost = ''
-# Generate 'lastMonth' 16th and 'currentMonth' day for endpoints
-now = Date.today()
+SCHEDULER.every '5h', :first_in => 0 do |job|
 
-from = Date.new( now.year, now.month - 1, 16 )
-to = Date.new( now.year, now.month, now.day )
+	# Create 'To' & 'From' for last bill cycle
+	now = Date.today()
 
+	# Should new apr 16 -> may 16
+	from = Date.new(now.year, now.month - 2 , 16)
+	to = Date.new(now.year, now.month - 1, 16)
 
-# Get Current Telephone usage Cost
-phone_report_url = zoomAPI( 'v1/report/getaudioreport', {
-	:from => from,
-	:to => to,
-	:page_size => 30,
-	:page_number => 1
-	} )
+	# Get some call records
+	url = zoomAPI( 'v1/report/getaudioreport', {:from=>from, :to=>to, :page_number=>1, :page_size=>6000} )
+	response = HTTParty.post(url)
+	total_records = response['total_records']
 
-call_report = HTTParty.post( phone_report_url )
-call_report = call_report.parsed_response
+	telephony_records = response['telephony_usage']
 
-records = call_report['total_records']
+	call_out_cost = 0.00
+	toll_free_cost = 0.00
+	sub_cost = 0.00
+	bill_overages = 0.00
 
+	if total_records > constants[:audio_commit_block] then 
 
-# Logic if calls are more than 3k
-if records > constants[:audio_commit_block].to_i then
+		telephony_records.each do |record|
+			if record['type'] == "call-out"
+				call_out_cost += record['total']
+			elsif record['type'] == "toll-free"
+				toll_free_cost += record['total']
+			end
+		end
 
-	print "records are bigger than commitment!!"
+		items_cost = constants.values.reduce(:+)
+		sub_cost = ( call_out_cost + toll_free_cost )
+		bill_overages = (sub_cost - 3000.00 + items_cost )
+		bill_overages = "%.02f" % bill_overages
+	else
+		estimated_cost = constants.values.reduce(:+)
+		estimated_cost =  "%.02f" % estimated_cost
 
-else
+	end
 
-	estimated_cost = constants.values.reduce(:+)
-	estimated_cost =  "%.02f" % estimated_cost
+	cpu = ( bill_overages.to_f / @active_users )
+	cpu = "%.02f" % cpu
 
-end
-binding.pry
-# Get cost to a var
+	totals = { :estimate => bill_overages, :costPerUser=>cpu}
 
-# Get all users that are currenly active
-
-user_url = zoomAPI( 'v1/report/getaccountreport',
-					{
-						:from=>from,
-						:to=>to,
-						:page_sie=>30,
-						:page_number=>page_number
-					}
-				 )
-	active_list = HTTParty.post( user_url )
-	active_list = active_list.parsed_response
-	active_users = active_list['total_records']
-
-# Divide Bill Estimate by Active users to = Cost Per User
+  send_event('bill_estimate', { final: totals } )
 
 
 
 
-SCHEDULER.every '1m', :first_in => 0 do |job|
 
 
 
-  send_event('widget_id', { })
+
+
+
 end
